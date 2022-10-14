04511fe802932b160392cfa68ea9f814b9324392
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-10-14 19:54:53 +1100

    Code steps init, skcode skeleton functions starter

--- chatbot.py
+++ chatbot.py
@@ -1,2 +1,22 @@
 """
+Chatbot Steps
+1. Input/Listen
+2. Process/Decide
+3. Output/Talkback
 """
+
+
+def listen():
+    return input("Say Something")
+
+
+def decide():
+    pass
+
+
+def talkback():
+    pass
+
+
+# Listening
+listen()
